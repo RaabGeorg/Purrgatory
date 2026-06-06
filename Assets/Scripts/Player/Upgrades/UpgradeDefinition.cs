91812d42894bbaee74f28c92a47c6145632df4a5
Didi-did-it <diart.tahiri@hotmail.com>
2026-06-06 12:54:45 +0200

    Small push Idk things are breaking

--- Assets/Scripts/Player/Upgrades/UpgradeDefinition.cs
+++ Assets/Scripts/Player/Upgrades/UpgradeDefinition.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "UpgradeDefinition", menuName = "Scriptable Objects/UpgradeDefinition")]
 public class UpgradeDefinition : ScriptableObject
 {
+    public Sprite icon;
     public string displayName;
     [TextArea] public string description;
     public RarityTier rarity;
